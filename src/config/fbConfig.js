4f4f51d45c8e597518133b4b7f46bc3b26467084
Emmanuel Elugbaju <elugbajuemmanueltobi@gmail.com>
2022-08-11 10:30:50 +0100

    Firebase / store Config

---
+++ src/config/fbConfig.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+
+import firebase from "firebase/app";
+import 'firebase/firestore';
+import 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBRAsHPEJQ4RGdacHJBJkJuXpJitFqWHUY",
+  authDomain: "plan-app-1a608.firebaseapp.com",
+  projectId: "plan-app-1a608",
+  storageBucket: "plan-app-1a608.appspot.com",
+  messagingSenderId: "422387884880",
+  appId: "1:422387884880:web:9cbb38fa8ad63bf67ac255"
+};
+
+// Initialize Firebase
+// const firebase = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({ timestampsInSnapshots: true});
+
+// export default firebase;
